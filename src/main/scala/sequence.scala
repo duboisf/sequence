a27417dbc2a8ab4993e3dcb05d40d777787a65f5
Fred Dubois <fred.dubois@gmail.com>
2010-05-28 05:55:16 -0400

    Next up, drawing the lifeline

--- src/main/scala/sequence.scala
+++ src/main/scala/sequence.scala
@@ -1,78 +1,88 @@
 package sequence
 
-import java.util._
+import java.util.Hashtable
 import java.awt.image.BufferedImage
 import java.awt._
 import java.awt.font._
 import java.awt.geom._
 import java.io.File
 import javax.imageio.ImageIO
+import scala.collection.mutable.ListBuffer
 
 trait Drawable {
-  def draw(g: Graphics2D): Unit
+  def draw(): Unit
 }
 
-class ObjectInstance (val name: String) extends Drawable {
-  var x: Int = 40
-  var y: Int = 40
+class ObjectInstance (val name: String, val x: Int, val y: Int, val g: Graphics2D) extends Drawable {
+  import ObjectInstance._
+  private val metrics = g.getFontMetrics(Canvas.font)
+  val height = metrics.getAscent
+  val width = metrics.stringWidth(name)
+  private val messages = new ListBuffer[Message]()
 
-  override def draw(g: Graphics2D) {
-    val metrics = g.getFontMetrics(Canvas.font)
-    val strHeight = metrics.getHeight
-    val strWidth = metrics.stringWidth(name)
-    g.fill(new RoundRectangle2D.Double(x, y, strWidth + 20, strHeight + 4, 30, 30))
-    g.drawString(name, x + 10, y + strHeight - 8)
+  override def draw() {
+    val oldColor = g.getColor
+    val stroke = g.getStroke
+    val objectStroke = new BasicStroke(3.0f)
+    g.setStroke(objectStroke)
+    val box = new RoundRectangle2D.Double(x, y, width + strWidthPad, height + strHeightPad, 0, 0)
+    g.setColor(Color.WHITE)
+    g.fill(box)
+    g.setColor(Color.BLACK)
+    g.draw(box)
+    g.setColor(oldColor)
+    g.drawString(name, x + strWidthPad / 2, y + height + strHeightPad / 2)
+    g.setStroke(stroke)
   }
+
+  def addMessage(msg: Message): Unit = messages += msg
 }
 
-class Message(val name: String, val from: ObjectInstance, val to: ObjectInstance) {
+object ObjectInstance {
+  private val strWidthPad = 20
+  private val strHeightPad = 20
+}
+
+class Message(val name: String, val x: Int, val y: Int, val to: ObjectInstance, val g: Graphics2D) extends Drawable {
+
+  override def draw() {
+    g.draw(new Line2D.Double(x, y, to.x + to.width / 2, y))
+  }
 }
 
 object Canvas {
   val gradient = new GradientPaint(0, 0, Color.BLUE, 200, 100, Color.WHITE);
-  val font = new Font("Arial", Font.TRUETYPE_FONT, 40)
+  val font = new Font("Arial", Font.TRUETYPE_FONT, 20)
+}
+
+class Canvas {
+
+  import Canvas._
 
   def draw() {
     val buffIm = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB)
     val g = buffIm.createGraphics()
 
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-    g.setPaint(gradient)
 
     val map = new Hashtable[TextAttribute, Object]()
     map.put(TextAttribute.FOREGROUND, Color.BLACK);
     
     g.setFont(font.deriveFont(map))
 
-    val inst = new ObjectInstance("Sequence")
-    inst.draw(g)
+    val inst1 = new ObjectInstance("Sequence", 0, 0, g)
+    val inst2 = new ObjectInstance("Message", inst1.x + inst1.width + 30, 0, g)
+    val msg1 = new Message("draw", inst1.x + inst1.width / 2, inst1.y + inst1.height + 30, inst2, g)
+    val l = scala.List(inst1, inst2, msg1)
+    l.map(_.draw)
     ImageIO.write(buffIm, "PNG", new File("test2.png"))
   }
 }
 
 object Sequence {
   def main(args: Array[String]) {
-    val inst = new ObjectInstance("fred")
-    val buffIm = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB)
-    val g = buffIm.createGraphics()
-    g.drawLine(20, 100, 120, 100)
-
-    val gradient = new GradientPaint(0, 0, Color.BLUE, 200, 100, Color.WHITE);
-
-    g.setPaint(gradient)
-    g.fill(new RoundRectangle2D.Double(10, 10, 200, 100, 30, 30))
-
-    val arial = new Font("Arial", Font.TRUETYPE_FONT, 40)
-    g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-    val map = new Hashtable[TextAttribute, Object]()
-    map.put(TextAttribute.FOREGROUND, Color.BLACK);
-    val blueArial = arial.deriveFont(map)
-    g.setFont(blueArial)
-    g.drawString("Hello, World!", 40, 300)
-//    GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames().map(println)
-//    val arial = Font.createFont(Font.TRUETYPE_FONT, new File("Arial.ttf"))
-    ImageIO.write(buffIm, "PNG", new File("test.png"))
-    Canvas.draw()
+    val canvas = new Canvas
+    canvas.draw()
   }
 }
 
