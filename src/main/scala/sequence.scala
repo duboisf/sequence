a30a7e394fc0cb887a8700abcf03e844dea24758
Fred Dubois <fred.dubois@gmail.com>
2010-05-29 22:21:19 -0400

    Refactored Message class

--- src/main/scala/sequence.scala
+++ src/main/scala/sequence.scala
@@ -33,8 +33,8 @@ trait Drawable {
   }
 }
 
-class ObjectInstance (val name: String, val left: Int, val top: Int, val g: Graphics2D) extends Drawable {
-  import ObjectInstance._
+class Instance (val name: String, val left: Int, val top: Int, val g: Graphics2D) extends Drawable {
+  import Instance._
   private val metrics = g.getFontMetrics(Canvas.font)
   private val stringHeight = metrics.getAscent
   private val stringWidth = metrics.stringWidth(name)
@@ -61,42 +61,78 @@ class ObjectInstance (val name: String, val left: Int, val top: Int, val g: Grap
   def addMessage(msg: Message): Unit = messages += msg
 }
 
-object ObjectInstance {
+object Instance {
   private val strWidthPad = 20
   private val strHeightPad = 20
 }
 
-class Message(val name: String, val y: Int, val from: ObjectInstance, val to: ObjectInstance, val g: Graphics2D) extends Drawable {
+object Direction extends Enumeration {
+  type Direction = Value
+  val East, West = Value
+}
 
-  override def draw() {
-    withPreservedState {
-      drawArrowHead
-      g.setColor(Color.BLACK)
-      g.setStroke(new BasicStroke(3.0f))
-      val destination = 
-        if (from.left < to.left)
-          to.middle - 15
-        else 
-          to.middle + 15
-      g.draw(new Line2D.Double(from.middle, y, destination, y))
-    }
+abstract class Message extends Drawable {
+  val name: String
+  val y: Int
+  val from: Instance
+  val to: Instance
+  val g: Graphics2D
+
+  import Direction._
+
+  protected def drawText(s: String, x: Int, y: Int) {
+
   }
 
-  private def drawArrowHead() {
+  protected def drawLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+    g.setColor(Theme.foregroundColor)
+    g.setStroke(Theme.lineStroke)
+    g.draw(new Line2D.Double(x1, y1, x2, y2))
+  }
+
+  protected def drawArrowHead(x: Int, y: Int, direction: Direction) {
     // Make arrow point right or left
-    val xs = 
-      if (from.left < to.left) 
-        Array(to.middle, to.middle - 15, to.middle - 15)
-      else 
-        Array(to.middle, to.middle + 15, to.middle + 15)
-    val ys = Array(y, y - 10, y + 10)
+    val arrowBase = direction match {
+      case East => x - Theme.arrowLength
+      case West => x + Theme.arrowLength
+      case direction_ => throw new IllegalArgumentException("Unknow direction '" + direction_)
+    }
+    val xs = Array(x, arrowBase, arrowBase)
+    val ys = Array(y, y - Theme.arrowHalfBaseWidth, y + Theme.arrowHalfBaseWidth)
     val arrow = new Polygon(xs, ys, xs.length)
     g.setStroke(new BasicStroke(1.0f))
     g.setColor(Color.BLACK)
     g.draw(arrow)
     g.fill(arrow)
   }
+}
 
+class ArrowMessage(
+  val name: String,
+  val y: Int,
+  val from: Instance,
+  val to: Instance,
+  val g: Graphics2D
+) extends Message {
+
+  import Direction._
+
+  override def draw() {
+    val op = (x: Int, y: Int) => if (from.left < to.left) x - y else x + y
+    val x2 = if (from.left < to.left) to.middle - Theme.arrowLength else to.middle + Theme.arrowLength
+    drawLine(from.middle, y, op(to.middle, Theme.arrowLength), y)
+    val direction = if (from.left < to.left) East else West
+    drawArrowHead(to.middle, y, direction)
+  }
+}
+
+object Theme {
+  val arrowLength = 15
+  val arrowHalfBaseWidth = 10
+  val lineStroke = new BasicStroke(3.0f)
+  val backgroundColor = Color.WHITE
+  val foregroundColor = Color.BLACK
+  val font = new Font("Arial", Font.TRUETYPE_FONT, 20)
 }
 
 object Canvas {
@@ -119,9 +155,9 @@ class Canvas {
     
     g.setFont(font.deriveFont(map))
 
-    val inst1 = new ObjectInstance("Sequence", 0, 0, g)
-    val inst2 = new ObjectInstance("Message", inst1.left + inst1.width + 30, 0, g)
-    val msg1 = new Message("draw", inst1.top + inst1.height + 30, inst2, inst1, g)
+    val inst1 = new Instance("Sequence", 0, 0, g)
+    val inst2 = new Instance("Message", inst1.left + inst1.width + 30, 0, g)
+    val msg1 = new ArrowMessage("draw", inst1.top + inst1.height + 30, inst2, inst1, g)
     val l = scala.List(inst1, inst2, msg1)
     l.map(_.draw)
     ImageIO.write(buffIm, "PNG", new File("test2.png"))
